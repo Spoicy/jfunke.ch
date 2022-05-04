474f5695007c36e02dfbb442221f1b551610b72d
spoicy <mrssfireball@gmail.com>
2022-05-05 00:08:58 +0200

    Add base template for every page

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+@include('components/section', [
+    'type' => 'full-width',
+    'blocks' => [
+        [
+            'block' => 'blocks/banner',
+            'type' => 'normal',
+            'imgsrc' => 'img/imgtest.png',
+            'imgalt' => 'Banner'
+        ]
+    ]
+])
