a15932b1294b27c8efb2bba35e25a60ecd26c304
spoicy <mrssfireball@gmail.com>
2022-08-31 23:13:19 +0200

    Fix portfoliocard not being clickable near the edge

--- resources/views/blocks/portfoliocard.blade.php
+++ resources/views/blocks/portfoliocard.blade.php
@@ -1,9 +1,11 @@
 <div class="block-portfoliocard">
     <div class="main-card">
         <a href="{{$data['link']}}" class="no-style">
-            <img src="{{$data['imgsrc']}}" alt="{{$data['imgalt']}}">
-            <h3>{{$data['name']}}</h3>
-            <p>{{$data['description']}}</p>
+            <div class="card-inner">
+                <img src="{{$data['imgsrc']}}" alt="{{$data['imgalt']}}">
+                <h3>{{$data['name']}}</h3>
+                <p>{{$data['description']}}</p>
+            </div>
         </a>
     </div>
 </div>
